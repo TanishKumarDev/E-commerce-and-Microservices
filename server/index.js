8deeff0f39a68b401913c634015bf0dd1a545ad0
Tanish Kumar <tanish.kumar.contact@gmail.com>
2025-06-05 09:15:47 +0530

    Add user authentication features with OTP verification, including user model, routes, and email sending functionality

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import connectDB from "./utils/db.js";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.js";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// using middleware to parse json - always use this before routes; because we are using json in the body of the request
+app.use(express.json());
+
+// using routes
+app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
